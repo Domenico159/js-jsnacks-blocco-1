991f31fb90b457ecc22fcd8fd9669f2a7a51de21
Domenico159 <domenico.zummo@gmail.com>
2021-03-25 13:30:05 +0100

    Esercizi snack completati

---
+++ Js/script.js
@@ -0,0 +1,172 @@
+
+
+
+// Esercizio 5 ******************************************************
+
+
+
+// var numeriA= [2,4,3,5];
+
+// var numeriB = [2,4,6,8,5,4,8,2,5,5,2,5,8,5,8,2,8];
+
+// var nRandom = Math.floor(Math.random() * 10);
+
+
+// console.log(numeriA);
+// console.log(numeriB);
+// console.log(numeriA.length);
+// console.log(numeriB.length);
+
+
+
+// while(numeriA.length !== numeriB.length){
+//     if(numeriA.length < numeriB.length){
+
+//         numeriA.push(nRandom);
+        
+//     }else if (numeriB.length < numeriA.length){
+    
+//         numeriB.push(nRandom);     
+    
+//     }
+// }
+    
+
+
+// if(numeriA.length === numeriB.length){
+//     console.log('I due Array sono uguali');
+// }
+
+
+// console.log(numeriA);
+// console.log(numeriB);
+// console.log(numeriA.length);
+// console.log(numeriB.length);
+
+
+
+
+
+
+
+
+
+
+// Esercizio 4 ********************************************************************
+
+
+
+//  var numeri = [2,5,74,8,2,4,65,12,8,9,3,5,7,4,2,5];
+
+//  var counter = 0;
+
+//  var sum = 0;
+
+//  var index = numeri.indexOf();
+
+//   while(counter < numeri.length){
+//       if(counter % 2 !== 0){
+//           sum += numeri[counter];
+//          console.log(numeri[counter]);
+//       }
+//       counter++;
+
+//   }
+
+//   console.log('Il totale è ' + sum);
+
+
+//  while(counter < numeri.length){
+//      if(index % 2 !== 0){
+//          console.log(numeri[counter]);
+//      }
+//      counter++;
+
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Esercizio 3****************************************************************
+
+
+
+//  var nomi = ['roberto', 'domenico','paolo','mauro'];
+
+//  var cognomi = ['zummo','duzioni','bianchi','rossi'];
+
+//  var invitati = [];
+
+//  while(invitati.length < 3){
+//     var n1 = Math.floor(Math.random() * nomi.length) ;
+//     var n2 = Math.floor(Math.random() * cognomi.length) ;
+//     if(n1 != n2){
+//      invitati.push(cognomi[n1] + ' ' + nomi[n2]);
+//     }
+//  }
+
+//  console.log(invitati);
+
+
+
+
+
+
+
+
+
+
+
+
+// Esercizio  2****************************************************************
+
+//  var number = parseInt( prompt('Inserisci un numero') );
+
+
+//  if(number % 2 === 0){
+//       console.log(number);
+//  }else{
+//      console.log(number + 1);
+//  }
+
+
+
+
+
+
+
+
+
+
+
+// Esercizio 1***********************************************************************
+
+
+
+//      var result= 0;
+    
+    
+    
+//      for(var i = 0 ; i < 5;i++){
+//          number = parseInt( prompt('Inserisci un numero') )
+//          console.log(number);
+//          result += number ;
+//      }
+    
+            
+
+//  console.log( 'la somma è ' + result);
+
